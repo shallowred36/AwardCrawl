3f9999c0d5d0d969bbac3041cfc9ef64ba7a7bcc
Yaotung Kuo (David) <yaokuo@qti.qualcomm.com>
2018-08-29 18:13:30 +0900

    Add preliminary code

--- award_crawl.py
+++ award_crawl.py
@@ -1,1 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
 
+html_text = requests.get("https://www.marriott.com/default.mi").text
+my_soup = BeautifulSoup(html_text, 'html.parser')
+print(my_soup.prettify())
