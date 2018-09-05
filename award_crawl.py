fb7793576cf7f02f2a6667bce241d5ea61ce38ee
Yaotung Kuo (David) <yaokuo@qti.qualcomm.com>
2018-09-05 18:39:17 +0900

    Add start and end dates

--- award_crawl.py
+++ award_crawl.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Obtain website from Chrome's webdriver
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=r"C:\Users\yaokuo\Documents\Personal - DO NOT BACKUP\Git\AwardCrawl\chromedriver_win32\chromedriver.exe")
@@ -18,10 +19,26 @@ except:
     print("cannot find destination input")
 """
 driver.implicitly_wait(3)
+
+# Obtain field prefix
 input_div = driver.find_element_by_css_selector(".tile-hsearch-homepage.m-homepage-hsearch.l-hsearch-2.l-hsearch-takeover.l-hsearch-cntnr.l-hsearch-bottom")
 prefix = input_div.get_attribute("data-id-prefix")
+
+# Obtain location field
 loc_input_field_name = prefix + "_search-location"
-#print(loc_input_field_name)
 loc_input_field = driver.find_element_by_id(loc_input_field_name)
 loc_input_field.send_keys("Maldives")
+
+# Obtain start date field
+from_date_field_name = prefix + "_hotel-fromDate"
+from_date_field = driver.find_element_by_id(from_date_field_name)
+from_date_field.clear()
+from_date_field.send_keys("Tue, Aug 6, 2019")
+
+# Obtain end date field
+to_date_field_name = prefix + "_hotel-toDate"
+to_date_field = driver.find_element_by_id(to_date_field_name)
+to_date_field.clear()
+to_date_field.send_keys("Sun, Aug 11, 2019")
+
 #driver.quit()
