799d8d9269542916786cdc4356342e429b67445a
shallowred36 <david.yt.kuo@gmail.com>
2018-09-26 01:59:26 +0900

    Add basic arguments parsing

--- award_crawl.py
+++ award_crawl.py
@@ -1,6 +1,11 @@
 import requests
 import argparse
+from datetime import datetime
 from bs4 import BeautifulSoup
+
+def parse(destination, check_in, check_out):
+    # Construct award search url
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -9,4 +14,16 @@ if __name__ == '__main__':
     parser.add_argument('check_in', help='Enter check-in date(YYYY/MM/DD)')
     parser.add_argument('check_out', help='Enter check-out date(YYYY/MM/DD)')
     args = parser.parse_args()
-    
+    # Extract arguments
+    destination = args.destination
+    check_in = datetime.strptime(args.chec_in, "%Y/%m/%d")
+    check_out = datetime.strptime(args.chec_out, "%Y/%m/%d")
+    today = datetime.now()
+    # Compare today's date with both check-in and check-out dates
+    if today < check_in and check_in < check_out:
+        print("Starting award night crawling...")
+        parse(destination, check_in, check_out)
+    elif today > check_in or today > check_out:
+        print("Please enter future check-in and check-out dates")
+    elif check_in > check_out:
+        print("Please enter a check-out date later than the check-in date")
